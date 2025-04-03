3a258cd3396ae47161a0a3aa0452d720bc559f56
Xinghan Yan <76619533+killua-y@users.noreply.github.com>
2025-04-02 20:21:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(
     cors({
         credentials: true,
-        origin: process.env.NETLIFY_URL || "http://localhost:5173",
+        origin: process.env.NETLIFY_URL,
     })
 );
 const sessionOptions = {

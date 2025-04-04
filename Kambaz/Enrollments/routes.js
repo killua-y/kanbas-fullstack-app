a491ccb34870ce08485ddd2abedd38de0081ada1
Xinghan Yan <76619533+killua-y@users.noreply.github.com>
2025-04-03 21:54:07 -0400

    Implement enrollment

--- Kambaz/Enrollments/routes.js
+++ Kambaz/Enrollments/routes.js
@@ -1,5 +1,17 @@
 import * as dao from "./dao.js";
 
 export default function EnrollmentRoutes(app) {
-    app.post("/api/enrollments", dao.enrollUserInCourse);
+    app.post("/api/enrollments/:courseId", (req, res) => {
+        const currentUser = req.session["currentUser"];
+        const { courseId } = req.params;
+        dao.enrollUserInCourse(currentUser._id, courseId);
+        res.json({ status: "success" });
+    });
+
+    app.delete("/api/enrollments/:courseId", (req, res) => {
+        const currentUser = req.session["currentUser"];
+        const { courseId } = req.params;
+        const status = dao.unenrollUserFromCourse(currentUser._id, courseId);
+        res.json(status ? { status: "success" } : { status: "error", message: "Enrollment not found" });
+    });
 }

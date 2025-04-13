320bb0c2da12d2da6ad8cf39bba33909adaf7a72
Xinghan Yan <76619533+killua-y@users.noreply.github.com>
2025-04-13 15:57:41 -0400

    fix worning

--- frontend/src/Piazza/components/ClassGlance.tsx
+++ frontend/src/Piazza/components/ClassGlance.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import './ClassGlance.css';
 import * as answerClient from '../Answer/client';
--- frontend/src/Piazza/components/TopNavigation.tsx
+++ frontend/src/Piazza/components/TopNavigation.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './TopNavigation.css';
 
 interface Category {
